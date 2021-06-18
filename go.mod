295910ec8a2836e4ad971125ec360ab19b750b16
Raoul Hecky <raoul.hecky@gmail.com>
2021-06-18 14:25:46 +0200

    Initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/raoulh/binky-nfc
+
+go 1.13
+
+require (
+	github.com/ebfe/scard v0.0.0-20190212122703-c3d1b1916a95 // indirect
+	github.com/raoulh/acr122u v0.0.0-20210615162825-83b38e550860
+)
---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/raoulh/acr122u"
+)
+
+func main() {
+	ctx, err := acr122u.EstablishContext()
+	if err != nil {
+		panic(err)
+	}
+
+	h := &handler{log.New(os.Stdout, "", 0)}
+
+	ctx.Serve(h)
+}
+
+type handler struct {
+	acr122u.Logger
+}
+
+func (h *handler) ServeCard(c acr122u.Card) {
+	h.Printf("%x\n", c.UID())
+}
+
+func (h *handler) CardRemoved() {
+	h.Printf("card removed\n")
+}
